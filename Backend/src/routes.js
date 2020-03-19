9186cbc2bfc85f6074a74f6004f073f1dfa6f2ff
victorleite40 <victorsalvetti@hotmail.com>
2020-03-19 14:36:15 -0300

    Created method to return board name

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -9,6 +9,8 @@ routes.get('/boards', BoardController.index);
 routes.post('/boards', BoardController.store);
 routes.delete('/boards', BoardController.destroy);
 
+routes.get('/boards/:board_id/name', BoardController.indexName);
+
 routes.get('/boards/:board_id', ItemController.index);
 routes.post('/boards/:board_id', ItemController.store);
 routes.put('/boards/:board_id', ItemController.update);
