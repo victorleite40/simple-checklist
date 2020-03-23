ea0e48be3919c7efa3614de2b0760d446a55ad6f
victorleite40 <victorsalvetti@hotmail.com>
2020-03-23 17:15:17 -0300

    Updated user model

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    email: String,
-    username: String,
-    password: String
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    }
 });
 
 module.exports = mongoose.model('User' , UserSchema);
